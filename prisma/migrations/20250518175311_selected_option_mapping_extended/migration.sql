d952ffe385a97b9a02d09be3b49b91c748491eda
vishal-Choudhary-hi <vishalchoudhary34510@gmail.com>
2025-06-05 22:27:48 +0530

    removing migrations from envs

--- prisma/migrations/20250518175311_selected_option_mapping_extended/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE `SelectedOptionMapping` ADD COLUMN `is_correct` BOOLEAN NULL DEFAULT false,
-    ADD COLUMN `manual_score` INTEGER NULL,
-    ADD COLUMN `skipped` BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20250601093821_score_in_selected_option/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `SelectedOptionMapping` ADD COLUMN `score` INTEGER NULL;
